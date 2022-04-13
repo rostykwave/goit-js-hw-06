21f632a61dffbcf84394e173c03028d95386ebbd
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-04-13 21:28:20 +0300

    counter ok

--- js/task-1.js
+++ js/task-1.js
@@ -5,15 +5,42 @@ const CounterPlugin = function ({
     this._value = initialValue;
     this._step = step;
 
-    this._getRefs(rootSelector);
+    this._refs = this._getRefs(rootSelector);
+
+    this._bindEvents();
+
+    this.updateValueUI();
 };
 
 CounterPlugin.prototype._getRefs = function (rootSelector) {
-    console.log(rootSelector);
-
+   
     const refs = {};
-    refs.container = document.querySelector('rootSelector');
+    refs.container = document.querySelector(rootSelector);
+    refs.incrementBtn = refs.container.querySelector('[data-increment]');
+    refs.decrementBtn = refs.container.querySelector('[data-decrement]');
+    refs.value = refs.container.querySelector('[data-value]');
+
+
+    return refs;
 }
+
+CounterPlugin.prototype._bindEvents = function () {
+    this._refs.incrementBtn.addEventListener('click', () => {
+        console.log(this);
+        this.increment();
+        this.updateValueUI();
+    });
+
+    this._refs.decrementBtn.addEventListener('click', () => {
+         console.log(this);
+        this.decrement();
+        this.updateValueUI();
+    });
+}
+
+CounterPlugin.prototype.updateValueUI = function () {
+    this._refs.value.textContent = this._value;
+ };
 
 CounterPlugin.prototype.increment = function () {
     this._value += this._step;
@@ -24,10 +51,13 @@ CounterPlugin.prototype.decrement = function () {
 }
 
 
-const counter1 = new CounterPlugin({rootSelector:'#counter-1', step: 10});
+// const counter1 = new CounterPlugin({rootSelector:'#counter-1', step: 7});
 
-console.log('counter1', counter1);
+// console.log('counter1', counter1);
 
-const counter2 = new CounterPlugin({rootSelector:'#counter-2', step: 13});
+// const counter2 = new CounterPlugin({rootSelector:'#counter-2', step: 13});
 
-console.log('counter2', counter2);
+// console.log('counter2', counter2);
+
+new CounterPlugin({ rootSelector: '#counter-1',initialValue: 200, step: 7 });
+new CounterPlugin({rootSelector:'#counter-2', step: 13});
