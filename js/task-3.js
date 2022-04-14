f3840fedc28fd1e5a56cb263940e60843495bd37
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-04-14 20:46:17 +0300

    3,4

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,10 @@
 import users from "./users.js";
 
 console.log(users);
+
+const getUsersWithGender = (users, gender) => {
+  // твій код
+    return users.filter(user => user.gender === gender);
+};
+
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
