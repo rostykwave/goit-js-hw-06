8dba49a2966ba1a2a3999aca73b8956052f21772
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-04-16 18:20:08 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,25 @@
 const inputRef = document.querySelector('#validation-input');
 
-function validInput(element) {
-    element.classList.add('valid');
+// function validInput(element) {
+//     element.classList.add('valid');
 
-     if (element.classList.contains('invalid')) {
-         element.classList.remove('invalid');
-    }
-}
-function inValidInput(element) {
-    element.classList.add('invalid');
+//      if (element.classList.contains('invalid')) {
+//          element.classList.remove('invalid');
+//     }
+// }
+// function inValidInput(element) {
+//     element.classList.add('invalid');
     
-    if (element.classList.contains('valid')) {
-        element.classList.remove('valid');
+//     if (element.classList.contains('valid')) {
+//         element.classList.remove('valid');
+//     }
+// }
+
+function inputValidation(element,add,remove) {
+    element.classList.add(add);
+    
+    if (element.classList.contains(remove)) {
+        element.classList.remove(remove);
     }
 }
 
@@ -19,9 +27,9 @@ function inputCheck() {
     console.log(`Введено ${this.value.length} символів`);
 
     if (this.value.length > this.dataset.length || this.value.length ===0) {
-        return inValidInput(this);
+        return inputValidation(this,'invalid','valid');
     } 
-    return validInput(this);
+    return inputValidation(this,'valid','invalid');
 };
 
 
