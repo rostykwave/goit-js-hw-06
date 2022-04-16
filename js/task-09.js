5c613404737127c9e7f4c910e2c645be05185a80
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-04-16 20:21:54 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  button: document.querySelector('button.change-color'),
+  colorValue: document.querySelector('span.color'),
+};
+
+function changeColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.colorValue.textContent  = getRandomHexColor();
+};
+
+
+
+refs.button.addEventListener('click', changeColor);
